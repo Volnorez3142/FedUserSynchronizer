a1425fabad1fa01f7915c85f8e99ca0162bfb411
Volnorez3142 <zaven.hambardzumyan@yandex.com>
2026-06-11 20:38:27 +0400

    Logging enhancement
    Better logging system, minor variable naming changes

--- FedUserSynchronizer.ps1
+++ FedUserSynchronizer.ps1
@@ -9,51 +9,66 @@
 
 #CHECKING AND CREATING THE NECESSARY DIRECTORIES, STARTING THE LOG
 $test3142dir = Test-Path C:\by3142
-$testpwnotifierdir = Test-Path C:\by3142\FedUserSynchronizer
+$fedusersyncdir = Test-Path C:\by3142\FedUserSynchronizer
 if (-not $test3142dir) {
     Write-Host "No C:\by3142 directory found!" -ForegroundColor Red
     Write-Host "Creating C:\by3142 directory..." -ForegroundColor DarkGreen
     New-Item -Path "c:\" -Name "by3142" -ItemType "directory"
     Write-Host "Creating C:\by3142\FedUserSynchronizer directory..." -ForegroundColor DarkGreen
     New-Item -Path "c:\by3142" -Name "FedUserSynchronizer" -ItemType "directory"
-} elseif (-not $testpwnotifierdir) {
+} elseif (-not $fedusersyncdir) {
     Write-Host "No C:\by3142\FedUserSynchronizer directory found!" -ForegroundColor Red
     Write-Host "Creating C:\by3142\FedUserSynchronizer directory..." -ForegroundColor DarkGreen
     New-Item -Path "c:\by3142" -Name "FedUserSynchronizer" -ItemType "directory"
 } else {
     #ALL FINE, SKIPPING
 }
-$pwnotifierpath = "C:\by3142\FedUserSynchronizer\"
+$fedusersyncpath = "C:\by3142\FedUserSynchronizer\"
 Start-Transcript -path C:\by3142\FedUserSynchronizer\SyncLog_$(Get-Date -Format "yyyy-MM-dd_HHmmss").txt -append
 
 #ENVRINONMENT VARIABLES
 $SearchBase = "OU=NONSAKURADA.LAN,OU=Users,OU=Armenia,OU=Sakurada Club,DC=sakurada,DC=lan" #THE OU OF WHICH USERS WILL BE AFFECTED
 $UserDescription = "REMOTE DOMAIN USER" #MUST BE ANYWHERE IN USER'S DESCRIPTION FIELD
 $RemoteDomain = "studio.lan"
-$Users = (Get-ADUser -Filter * -SearchBase $SearchBase -Properties Description,Office | Select-Object Name,SamAccountName,Description,Office) | Where-Object { $_.Description -like "*$($UserDescription)*" }
+$Users = Get-ADUser -Filter * -SearchBase $SearchBase -Properties Description,Office | Select-Object Name,SamAccountName,Description,Office | Where-Object { $_.Description -like "*$($UserDescription)*" }
 
 #CORE LOGIC
 $Users | ForEach-Object {
-    if ($_.Description -like "*$($UserDescription)*") { 
-        $LocalUser = Get-ADUser $_.SamAccountName -Properties Office
-        Write-Host "Working on $($LocalUser.Name)..."
-        $RemoteUser = Get-ADUser $LocalUser.Office -Server $RemoteDomain -Properties Name,SamAccountName,Title,Department,Enabled | Select-Object Name,SamAccountName,Title,Department,Enabled
+    $LocalUser = Get-ADUser $_.SamAccountName -Properties Office
+    Write-Host "Working on $($LocalUser.Name)..."
+    $Error.Clear()
+    Try {
+        $global:RemoteUser = Get-ADUser $LocalUser.Office -Server $RemoteDomain -Properties Name,SamAccountName,Title,Department,Enabled | Select-Object Name,SamAccountName,Title,Department,Enabled
         #THE OFFICE ATTRIBUTE SHOULD CONTAIN USER'S SAMACCOUNT NAME ON THE REMOTE DOMAIN
+    } catch {
+        Write-Host "$($global:LocalUser.Name) [$($LocalUser.Office)] was not found in $($RemoteDomain)! Disabling $($global:LocalUser.SamAccountName) locally!" -ForegroundColor Red
+        Disable-ADAccount -Identity $global:LocalUser.SamAccountName
+        $global:ErrorUserList += "$($LocalUser.Name) | " + "$($LocalUser.SamAccountName) at $($RemoteDomain) `n"
+    }
+    if (!$error) {
         if ($RemoteUser.Enabled -eq $null) { #IF USER IS ENABLED, THE CELL WILL COME BACK EMPTY
-            Write-Host "$($RemoteUser.Name) [$($RemoteUser.SamAccountName)] is enabled on $($RemoteDomain), enabling $($LocalUser.Name) [$($LocalUser.SamAccountName)] locally." -ForegroundColor DarkGreen
-            Enable-ADAccount -Identity $LocalUser.SamAccountName
-            Write-Host "Syncrhonizing job title and department names..." -ForegroundColor Cyan
-            Set-ADUser -Identity $LocalUser.SamAccountName -Title $RemoteUser.Title -Department $RemoteUser.Department
-            Write-Host "Job title:  $((Get-ADUser $LocalUser.SamAccountName -Properties Title).Title)" -ForegroundColor Gray
-            Write-Host "Department: $((Get-ADUser $LocalUser.SamAccountName -Properties Department).Department)" -ForegroundColor Gray
+        Write-Host "$($RemoteUser.Name) [$($RemoteUser.SamAccountName)] is enabled on $($RemoteDomain), enabling $($LocalUser.Name) [$($LocalUser.SamAccountName)] locally." -ForegroundColor DarkGreen
+        Enable-ADAccount -Identity $LocalUser.SamAccountName
+        Write-Host "Syncrhonizing job title and department names..." -ForegroundColor Cyan
+        Set-ADUser -Identity $LocalUser.SamAccountName -Title $RemoteUser.Title -Department $RemoteUser.Department
+        Write-Host "Job title:  $((Get-ADUser $LocalUser.SamAccountName -Properties Title).Title)" -ForegroundColor Gray
+        Write-Host "Department: $((Get-ADUser $LocalUser.SamAccountName -Properties Department).Department)" -ForegroundColor Gray
         } elseif ($RemoteUser.Enabled -eq $False) {
             Write-Host "$($RemoteUser.Name) [$($RemoteUser.SamAccountName)] is disabled on $($RemoteDomain), disabling $($LocalUser.Name) [$($LocalUser.SamAccountName)] locally." -ForegroundColor Red
             Disable-ADAccount -Identity $LocalUser.SamAccountName
         }
-        Write-Output "================"
-        Write-Output " "
+    } elseif ($error) {
+        #SKIPPING
     }
+    Write-Output "================"
+    Write-Output " "
 }
+
+Write-Host @"
+USERS WITH ERROR LIST:
+$ErrorUserList 
+================
+"@
 
 #STOPPING THE TRANSCRIPT
 $by3142 = @"
