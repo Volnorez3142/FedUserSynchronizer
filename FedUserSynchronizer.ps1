37467cf59e37d06e896afc01eb66f7089466f5f4
Volnorez3142 <zaven.hambardzumyan@yandex.com>
2026-07-07 14:19:03 +0400

    Email notifier and error-stop mechanisms added
    From now on, you can add alerting system through SMTP. Also, there is a condition on when the code stops in the try-catch method: IF the user isn't found on the remote domain, it's disabled locally, in any other case the script stops immediately, logging the error and sending email alerts.

--- FedUserSynchronizer.ps1
+++ FedUserSynchronizer.ps1
@@ -23,14 +23,29 @@ if (-not $test3142dir) {
 } else {
     #ALL FINE, SKIPPING
 }
-$fedusersyncpath = "C:\by3142\FedUserSynchronizer\"
-Start-Transcript -path C:\by3142\FedUserSynchronizer\SyncLog_$(Get-Date -Format "yyyy-MM-dd_HHmmss").txt -append
+$fedusersyncpath = "C:\by3142\FedUserSynchronizer"
+$rightnow = $(Get-Date -Format "yyyy-MM-dd_HHmmss")
+$prettydate = (Get-Date)
+$logpath = "$($fedusersyncpath)\SyncLog_$($rightnow).txt"
+Start-Transcript -path $logpath -append
 
 #ENVRINONMENT VARIABLES
 $SearchBase = "OU=NONSAKURADA.LAN,OU=Users,OU=Armenia,OU=Sakurada Club,DC=sakurada,DC=lan" #THE OU OF WHICH USERS WILL BE AFFECTED
 $UserDescription = "REMOTE DOMAIN USER" #MUST BE ANYWHERE IN USER'S DESCRIPTION FIELD
 $RemoteDomain = "studio.lan"
 $Users = Get-ADUser -Filter * -SearchBase $SearchBase -Properties Description,Office | Select-Object Name,SamAccountName,Description,Office | Where-Object { $_.Description -like "*$($UserDescription)*" }
+$emailnotify = 1 #IF 1, EMAIL NOTIFICATIONS WILL BE SENT, ELSE = THEY WON'T
+
+#CREDS AND AUTH INFO FOR SMTP
+$smtplogin = "3142@volnorez.lan"
+$smtppw = "YOUR_PASSWORD"
+$credentials = New-Object System.Management.Automation.PSCredential(
+    $smtplogin,
+    (ConvertTo-SecureString $smtppw -AsPlainText -Force)
+)
+$smtpserver = "smtp.volnorez.lan"
+$smtpport = 587
+$admins = "zaven@volnorez.lan","nikita@volnorez.lan"
 
 #CORE LOGIC
 $Users | ForEach-Object {
@@ -40,10 +55,33 @@ $Users | ForEach-Object {
     Try {
         $global:RemoteUser = Get-ADUser $LocalUser.Office -Server $RemoteDomain -Properties Name,SamAccountName,Title,Department,Enabled | Select-Object Name,SamAccountName,Title,Department,Enabled
         #THE OFFICE ATTRIBUTE SHOULD CONTAIN USER'S SAMACCOUNT NAME ON THE REMOTE DOMAIN
-    } catch {
+    } catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
         Write-Host "$($global:LocalUser.Name) [$($LocalUser.Office)] was not found in $($RemoteDomain)! Disabling $($global:LocalUser.SamAccountName) locally!" -ForegroundColor Red
         Disable-ADAccount -Identity $global:LocalUser.SamAccountName
         $global:ErrorUserList += "$($LocalUser.Name) | " + "$($LocalUser.SamAccountName) at $($RemoteDomain) `n"
+    } catch {
+        $Exception = "SCRIPT UNSPECIFIED ERROR:
+        EXCEPTION NAME: $($_.Exception.GetType().FullName)
+        EXCEPTION MESSAGE: $($_.Exception.Message)
+        EXCEPTION ID: $($_.FullyQualifiedErrorId)"
+        Write-Host $Exception -ForegroundColor DarkRed
+        if ($emailnotify -eq 1) {
+            Write-Host "Sending email to: $($admins)" -ForegroundColor Green
+                    Send-MailMessage -To $admins `
+                        -From $smtplogin `
+                        -Subject "ERROR: FedUserSyncrhonizer" `
+                        -Body "FedUserSynchronizer on $($env:COMPUTERNAME) failed to run at $($prettydate)!
+            Check $($logpath) for the error logs! `n`
+            EXCEPTION DETAILS: $($Exception)" `
+                        -SmtpServer $smtpserver `
+                        -Port $smtpport `
+                        -Credential $credentials `
+                        -UseSsl
+        } elseif ($emailnotify -ne 1) {
+            #SKIPPING
+        }
+        Write-Host "Exiting!" -ForegroundColor Red
+        Exit
     }
     if (!$error) {
         if ($RemoteUser.Enabled -eq $null) { #IF USER IS ENABLED, THE CELL WILL COME BACK EMPTY
